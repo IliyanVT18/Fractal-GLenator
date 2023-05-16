349f49e35f1fdb2968eaf918ed2aca63dc7646ce
Iliyan Teofilov <iliyanvt18@gmail.com>
2023-05-16 17:31:26 +0200

    Made two triangles that span the whole screen area

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,20 @@
 #define WIDTH 800
 #define HEIGHT 600
 
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+
 int main(void)
 {
     GLFWwindow* window;
@@ -26,25 +40,65 @@ int main(void)
     glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
 
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
+       -1.0f, -1.0f, 0.0f, // top left
+        1.0f, -1.0f, 0.0f, // top right
+       -1.0f,  1.0f, 0.0f, // bottom left
+        1.0f,  1.0f, 0.0f  // bottom right
     };  
-	uint32_t v;
-	glGenBuffers(1, &v);
-	glBindBuffer(GL_ARRAY_BUFFER, v);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices)*sizeof(vertices)/sizeof(vertices[0]),
-		&vertices[0], GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*3,
-		 (void*)0);
+
+    uint32_t indices[] = {
+        0, 1, 2,    // first triangle
+        1, 2, 3     // second triangle
+    };
+    
+    uint32_t vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    uint32_t fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    uint32_t VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // copy our vertex data to a vertex buffer for opengl to use
+	uint32_t VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    uint32_t EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // set vertex attribute pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*3, (void*)0);
 	glEnableVertexAttribArray(0);
-	glViewport(0, 0, 600, 600);
+
+    uint32_t shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // we don't need the shaders anymore once we've linked them
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
         glClear(GL_COLOR_BUFFER_BIT);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
